bbf3e26d0f226fb3b7cb0a452d3babdfb7f2cd55
hughpu <hughpu@hotmai.com>
2023-12-16 13:42:13 +0800

    feat: add `ShareQueueThreadPool`

--- src/thread_pool.rs
+++ src/thread_pool.rs
@@ -1,5 +1,6 @@
 use crate::error::Result;
-use std::thread;
+use std::{thread, sync::{atomic::{AtomicBool, Ordering}, Arc}};
+use crossbeam::channel::{bounded, Sender, Receiver};
 
 /// thread pool trait
 pub trait ThreadPool {
@@ -21,6 +22,59 @@ impl ThreadPool for NaiveThreadPool {
     
     fn spawn<F>(&self, job: F) where F: FnOnce() + Send + 'static {
         thread::spawn(job);
+    }
+}
+
+type DynFunc = Box<dyn FnOnce() + Send + 'static>;
+
+/// the shared queue thread pool
+pub struct SharedQueueThreadPool
+{
+    sender: Sender<DynFunc>,
+    terminated: Arc<AtomicBool>,
+    threads: u32,
+}
+
+impl ThreadPool for SharedQueueThreadPool
+{
+    fn new(threads: u32) -> Result<Self> {
+        let (sender, receiver) = bounded::<DynFunc>((threads * 2) as usize);
+        let terminated = Arc::new(AtomicBool::new(false));
+        
+        for _ in 0..threads {
+            let cur_terminated = terminated.clone();
+            let cur_receiver = receiver.clone();
+            thread::spawn(move|| {
+                while !cur_terminated.load(Ordering::SeqCst) {
+
+                    if let Ok(job) = cur_receiver.recv() {
+                        job();
+                    }
+                }
+            });
+        }
+
+        return Ok(
+            Self {
+                sender,
+                terminated,
+                threads,
+            });
+    }
+    
+    fn spawn<F>(&self, job: F) where F: FnOnce() + Send + 'static {
+        let _ = self.sender.send(Box::new(job));
+    }
+}
+
+impl SharedQueueThreadPool {
+    pub fn stop(&self) { let cur_terminated = self.terminated.clone();
+        cur_terminated.store(true, Ordering::SeqCst);
+        for _ in 0..self.threads {
+            let _ = self.sender.send(
+                Box::new(|| {})
+            );
+        }
     }
 }
 
